899d1e72d2da62b1a7f77d508f2a974582e75894
Nebuchadnezzar-I <onur.orrmenn@gmail.com>
2024-11-06 18:16:27 +0100

    NextJS swap

--- src/vite-env.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="vite/client" />
-export const VITE_SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
-export const VITE_SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY
-export const OPEN_AI_API_KEY = import.meta.env.OPEN_AI_API_KEY
--- todo.md
+++
@@ -1,5 +0,0 @@
-- [-] Add state machine to form
-- [ ] Save form to DB
-- [ ] Add list of negotiations
-- [ ] Create form to add session
-- [ ] Implement session messaging
